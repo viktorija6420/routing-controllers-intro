482e1392c1078ee05c3aa2ded0b749c999ca429d
viktorija6420 <viktorija.gjorgjievska@gmail.com>
2017-08-30 15:24:14 -0400

    @header has now different values in different instance methods

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,14 @@
 class PagesController < ApplicationController
 
   def welcome
-    # render :about
+   @header = "This is the welcome page"  # render :about
   end
 
   def about
+    @header = "This is where you read about whoever owns this site"
   end
 
   def contest
+    @header = "Get ready for a contest!"
   end
 end
